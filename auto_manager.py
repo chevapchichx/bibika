8471d7a8d4aa21e266bdfba38b97fe367e489922
chevapchichx <tugaisveta@gmail.com>
2024-05-16 18:46:06 +0300

    Initial commit

---
+++ auto_manager.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+from PIL import Image, ImageTk
+
+
+def clck_lk():
+   auth_lk()
+
+
+def auth_lk():
+    window_lk = Tk()
+    window_lk.title("lk")
+    window_lk.geometry('350x150')
+
+    frame = Frame(
+       window_lk,
+       padx=10,
+       pady=10
+    )
+    frame.pack(expand=True)
+
+    login_lb = Label(
+       frame,
+       text="Логин менеджера"
+    )
+    login_lb.grid(row=3, column=1)
+
+    login_tf = Entry(
+       frame,
+    )
+    login_tf.grid(row=3, column=2)
+
+    password_lb = Label(
+       frame,
+       text="Пароль менеджера"
+    )
+    password_lb.grid(row=4, column=1)
+
+    password_tf = Entry(
+       frame,
+    )
+    password_tf.grid(row=4, column=2)
+
+    ent_btn = Button(
+       frame,
+       text="Войти",
+    )
+    ent_btn.grid(row=5, column=2)
+
+    window_lk.mainloop()
+
+
+with sqlite3.connect("auto_shop.db") as BD:
+    cursor = BD.cursor()
+    cursor.execute("SELECT ID_M, FIO, Phone_num, Login FROM manager WHERE Login =  ")
+    users = cursor.fetchall()
+    for user in users:
+        if Login == str(users[0]):
+            if Password == str(users[1]):
+                messagebox.showinfo('Успешно!', 'Авторизация успешно прошла')
+
+
+        messagebox.showinfo('Ошибка', 'Неверный логин или пароль')
+
+
+
+
---
+++ auto_shop.py
@@ -0,0 +1,67 @@
+import sqlite3
+import tkinter as tk
+
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import auto_manager
+
+con = sqlite3.connect("auto_shop.db")
+
+window = Tk()
+window.title("bibika.ru")
+window.geometry('850x600')
+
+frame = Frame(
+   window,
+   padx=10,
+   pady=10
+)
+frame.pack(expand=True)
+
+# image = Image.open("main_photo.jpg")
+# photo = ImageTk.PhotoImage(image)
+# label = Label(window, image=photo)
+# label.pack()
+
+path = 'main_photo.jpg'
+
+image = Image.open(path)
+width = 850
+height = 600
+image = ImageTk.PhotoImage(image)
+
+canvas = tk.Canvas(window, width=width, height=height)
+canvas.pack(side="top", fill="both", expand="no")
+canvas.create_image(0, 0, anchor="nw", image=image)
+
+auto_go_btn = Button(
+   window,
+   text="Выбрать бибику",
+   command=window.quit,
+   bg="#CDAA7D",
+   fg="#6E7B8B",
+   font=("comic sans", 16),
+   relief="flat",
+   borderwidth=0,
+)
+canvas.create_window((90, 500), anchor="nw", window=auto_go_btn)
+
+manager_go_btn = Button(
+   window,
+   text="Личный кабинет",
+   command=auto_manager.clck_lk,
+   bg="#CDAA7D",
+   fg="#6E7B8B",
+   font=("comic sans", 11),
+   relief="flat",
+   borderwidth=0,
+)
+canvas.create_window((30, 10),anchor="nw", window=manager_go_btn, width=97, height=25)
+
+window.mainloop()
+
+
+
+
+
