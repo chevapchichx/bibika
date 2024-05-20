883630342875a4e388cfc8589339dd90d715ed01
chevapchichx <tugaisveta@gmail.com>
2024-05-20 23:47:17 +0300

    no god in this room

--- auto_manager.py
+++ auto_manager.py
@@ -1,5 +1,5 @@
 import sqlite3
-from tkinter import Tk, Toplevel, Frame, Label, Entry, Button, messagebox
+from tkinter import Toplevel, Frame, Label, Entry, Button, messagebox
 import tkinter as tk
 
 
@@ -10,135 +10,73 @@ def toggle_password(password_entry):
         password_entry.config(show='')
 
 
-def auth_lk_window(my_bibiki_button):
+def auth_lk_window(add_my_bibiki_button):
     window_lk = Toplevel()
     window_lk.title("lk")
     window_lk.geometry('360x140')
     window_lk.grab_set()
     window_lk.resizable(False, False)
 
-    frame = Frame(
-        window_lk,
-        padx=0,
-        pady=0
-    )
+    frame = Frame(window_lk, padx=0, pady=0)
     frame.pack(expand=True)
 
-    login_lb = Label(
-        frame,
-        text="Логин менеджера",
-        fg='#FFF5EE'
-
-    )
+    login_lb = Label(frame, text="Логин менеджера")
     login_lb.grid(row=3, column=1)
 
-    password_lb = Label(
-        frame,
-        text="Пароль менеджера",
-        fg='#FFF5EE',
-    )
+    password_lb = Label(frame, text="Пароль менеджера")
     password_lb.grid(row=4, column=1)
 
-    login_entry = Entry(
-        frame,
-    )
+    login_entry = Entry(frame)
     login_entry.grid(row=3, column=2)
 
-    password_entry = Entry(
-        frame,
-        show="*",
-        fg='#FFF5EE'
-    )
+    password_entry = Entry(frame, show="*")
     password_entry.grid(row=4, column=2)
 
     show_pass_checkbox = tk.Checkbutton(frame, text="Показать пароль", command=lambda: toggle_password(password_entry))
     show_pass_checkbox.grid(row=5, column=2)
 
-    def open_info_man_window():
-        get_auth(login_entry, password_entry, window_lk, my_bibiki_button)
-
-    ent_btn = Button(
-        frame,
-        text="Войти",
-        command=open_info_man_window,
-        bg='#FFF5EE',
-        fg='#6E7B8B'
-
-    )
+    ent_btn = Button(frame, text="Войти",
+                     command=lambda: [add_my_bibiki_button(login_entry.get(), password_entry.get()),
+                                      window_lk.destroy()])
     ent_btn.grid(row=6, column=2)
 
-    window_lk.mainloop()
 
-
-def get_auth(login_entry, password_entry, window_lk, my_bibiki_button):
-    login = login_entry.get()
-    password = password_entry.get()
+def get_auth(login, password):
     with sqlite3.connect("db/auto_shop.db") as BD:
         cursor = BD.cursor()
         cursor.execute("SELECT FIO, Phone_num, Login, Password FROM manager WHERE Login=? AND Password=?",
                        (login, password))
         user = cursor.fetchone()
         if user:
-            for widget in window_lk.winfo_children():
-                widget.destroy()
-            info_man_window(window_lk, user, my_bibiki_button)
+            return user
         else:
             messagebox.showinfo('Ошибка', 'Неверный логин или пароль')
+            return None
 
 
-def info_man_window(window_lk, user, my_bibiki_button):
-    window_lk.title("info manager")
+def info_man_window(user):
+    window_lk = Toplevel()
+    window_lk.title("Информация о менеджере")
     window_lk.geometry('360x140')
-    window_lk.grab_set()
     window_lk.resizable(False, False)
 
-    frame = Frame(
-        window_lk,
-        padx=0,
-        pady=0,
-    )
+    frame = Frame(window_lk, padx=0, pady=0)
     frame.pack(expand=True)
 
-    info_man_lb = Label(
-        frame,
-        text="Информация о менеджере",
-        font=("comic sans", 16),
-        anchor="center",
-        fg='#FFF5EE'
-    )
+    info_man_lb = Label(frame, text="Информация о менеджере", font=("comic sans", 16), anchor="center", fg='#FFF5EE')
     info_man_lb.grid(row=0, column=0, columnspan=3, sticky=tk.W + tk.E + tk.N)
 
     space_lb = Label(frame, text="", fg='#FFF5EE')
     space_lb.grid(row=1, column=0, columnspan=3)
 
-    fio_man_lb = Label(
-        frame,
-        text="ФИО",
-        fg='#FFF5EE'
-    )
+    fio_man_lb = Label(frame, text="ФИО", fg='#FFF5EE')
     fio_man_lb.grid(row=2, column=0, columnspan=1, sticky=tk.W)
 
-    fio_man_db = Label(
-        frame,
-        text=user[0],
-        fg='#FFF5EE'
-    )
+    fio_man_db = Label(frame, text=user[0], fg='#FFF5EE')
     fio_man_db.grid(row=2, column=1, columnspan=1, sticky=tk.E)
 
-    phone_man_lb = Label(
-        frame,
-        text="Номер телефона",
-        fg='#FFF5EE'
-    )
+    phone_man_lb = Label(frame, text="Номер телефона", fg='#FFF5EE')
     phone_man_lb.grid(row=3, column=0, columnspan=1, sticky=tk.W)
 
-    phone_man_db = Label(
-        frame,
-        text=user[1],
-        fg='#FFF5EE'
-    )
+    phone_man_db = Label(frame, text=user[1], fg='#FFF5EE')
     phone_man_db.grid(row=3, column=1, columnspan=1, sticky=tk.E)
-
-    my_bibiki_button()
-
-# auth_lk_window()
--- auto_shop.py
+++ auto_shop.py
@@ -1,9 +1,27 @@
 import tkinter as tk
+import sqlite3
 from PIL import Image, ImageTk
-from tkinter import Tk, Toplevel, Frame, Button, Label
+from tkinter import Tk, Frame, Button
 
 import auto_bibiki
 import auto_manager
+
+global window_main, canvas
+
+
+def add_my_bibiki_button(window_main, canvas, login, password):
+    user = auto_manager.get_auth(login, password)
+    if user:
+        my_bibiki_btn = Button(
+            window_main,
+            text=f"{user[2]}",
+            command=lambda: auto_manager.info_man_window(user),
+            fg="#6E7B8B",
+            font=("comic sans", 11),
+            relief="flat",
+            borderwidth=0,
+        )
+        canvas.create_window((820, 10), anchor="ne", window=my_bibiki_btn, width=120, height=25)
 
 
 def main_window():
@@ -24,24 +42,6 @@ def main_window():
     canvas.pack(side="top", fill="both", expand="no")
     canvas.create_image(0, 0, anchor="nw", image=image)
 
-    def open_my_bibiki_window():
-        for widget in window_main.winfo_children():
-            widget.destroy()
-        auto_bibiki.bibiki_change_window(window_main, main_window)
-
-    def my_bibiki_button():
-        my_bibiki_btn = Button(
-            window_main,
-            text="Мои бибики",
-            command=open_my_bibiki_window,
-            bg="#CDAA7D",
-            fg="#6E7B8B",
-            font=("comic sans", 11),
-            relief="flat",
-            borderwidth=0,
-        )
-        canvas.create_window((820, 10), anchor="ne", window=my_bibiki_btn, width=97, height=25)
-
     def open_bibiki_contents_window():
         for widget in window_main.winfo_children():
             widget.destroy()
@@ -51,7 +51,6 @@ def main_window():
         window_main,
         text="Выбрать бибику",
         command=open_bibiki_contents_window,
-        bg="#CDAA7D",
         fg="#6E7B8B",
         font=("comic sans", 16),
         relief="flat",
@@ -62,21 +61,16 @@ def main_window():
     manager_go_btn = Button(
         window_main,
         text="Личный кабинет",
-        command=lambda: auto_manager.auth_lk_window(my_bibiki_button),
-        bg="#CDAA7D",
+        command=lambda: auto_manager.auth_lk_window(
+            lambda login, password: add_my_bibiki_button(window_main, canvas, login, password)),
         fg="#6E7B8B",
         font=("comic sans", 11),
         relief="flat",
         borderwidth=0,
     )
-    canvas.create_window((30, 10), anchor="nw", window=manager_go_btn, width=97, height=25)
+    canvas.create_window((30, 10), anchor="nw", window=manager_go_btn, width=115, height=25)
 
     window_main.mainloop()
 
 
 main_window()
-
-# def open_info_man_windoww():
-#     for widget in window_main.winfo_children():
-#         widget.destroy()
-#     auto_manager.info_man_window(window_main, user=None, my_bibiki_button=my_bibiki_button)
